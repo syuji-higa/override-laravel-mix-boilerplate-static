8c27d511fab712be1b52d666bc37a6cfc1a473ae
比嘉 秀士 <higa_syuuji@liginc.co.jp>
2019-03-15 19:13:21 +0900

    update

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,6 +1,9 @@
+import ClientFlagSetter from './views/client-flag-setter'
 import { windowSizeObserver } from './module'
 import Lazyloader from './views/lazyloader'
 import HeightFitter from './views/height-fitter'
+
+new ClientFlagSetter()
 
 windowSizeObserver.on().resize()
 
---
+++ resources/assets/js/views/client-flag-setter.js
@@ -0,0 +1,11 @@
+import { store } from '../store'
+
+class ClientFlagSetter {
+  constructor() {
+    const { platform } = store.state
+
+    document.documentElement.classList.add(`is-${platform.type}`)
+  }
+}
+
+export { ClientFlagSetter as default }
